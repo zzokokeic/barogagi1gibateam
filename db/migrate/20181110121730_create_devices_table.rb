dbf6eba55fe9a413eb43e6111cc66100244f84af
JinJuLee <pearl1379@gmail.com>
2018-11-10 22:54:55 +0900

    DB update

--- db/migrate/20181110121730_create_devices_table.rb
+++ db/migrate/20181110121730_create_devices_table.rb
@@ -2,7 +2,7 @@ class CreateDevicesTable < ActiveRecord::Migration[5.2]
   def change
   	create_table :devices do |d|
   		d.integer	:user_id
-  		d.uuid	:token
+  		d.string	:token
   		d.datetime	:created_at
   	end
   end

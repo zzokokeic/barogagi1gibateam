79a19a9a9ab4986a79c724a83d25baeb536bdcac
JinJuLee <pearl1379@gmail.com>
2018-11-25 01:22:56 +0900

    review with duhee

--- controller.rb
+++ controller.rb
@@ -19,6 +19,7 @@ post '/sign_up' do
         return "error_1_5".to_json # Enter Upgrade_password
     end
 
+    # substring => @ and .
     unless User.where("nickname" => params["nickname"]).take.nil?
         return "error_1_6".to_json # Nickname is in use. Enter another nickname.
     end
@@ -69,8 +70,7 @@ post '/sign_in' do # error_2
     if user.nil?
         return "error_2_1".to_json # 
     end
-
-    puts user.password
+    
     unless BCrypt::Password.new(user.password) == params["password"]
         return "error_2_2".to_json # 
     end
@@ -81,26 +81,6 @@ post '/sign_in' do # error_2
     device.save
     return device.to_json
 end
-
-=begin
-    include BCrypt
-
-    def password
-        @password ||= Password.new(password_hash)
-    end
-
-    def password=(new_password)
-        @password = Password.create(new_password)
-        self.password_hash = @password
-    end
-end
-=end
-
-# 암호화하는 방식(BCrypt)
-# require 'bcrypt'
-# a = BCrypt::Password.create("1234")
-# BCrypt::Password.new(a) == "1234"
-
 
 # duhee
 post '/buy_coin' do #error3
@@ -119,11 +99,7 @@ end
 
 # sori
 get '/get_animal_list' do  #error5
-    if Animal.nil?
-        return "error_5_1"
-    else
-        return Animal.all.to_json
-    end
+    return Animal.all.to_json
 end
 
 # jinju
@@ -131,7 +107,7 @@ post '/get_my_animal_list' do #error6
     user = Device.find_by_token(params["token"]).user
 
     if user.nil?
-        return "error6_1".to_json
+        return "error_6_1".to_json
     else
         animals = user.myanimals
         if animals.nil?
@@ -152,11 +128,7 @@ end
 
 # sori
 get '/get_habit_list' do #error7
-    if Habit.nil?
-        return "error_7_1"
-    else
-        return Habit.all.to_json
-    end
+    return Habit.all.to_json
 end
 
 # jinju
@@ -166,14 +138,14 @@ post '/buy_animal' do #error8
     habit = Habit.find(params["habit_id"])
 
     if user.nil?
-        return "error8_1".to_json
+        return "error_8_1".to_json
     elsif animal.nil?
-        return "error8_2".to_json
+        return "error_8_2".to_json
     elsif habit.nil?
-        return "error8_3".to_json
+        return "error_8_3".to_json
     else
         if user.current_coin < animal.coin_price
-            return "error8_4".to_json 
+            return "error_8_4".to_json 
         else
             myanimal = Myanimal.new
             myanimal.user = user
@@ -205,7 +177,7 @@ post '/animal_upgrade' do #error9
         return "error_9".to_json #wrong upgrade password
     end
 
-    if myanimal.growth_step = myanimal.animal.max_step 
+    if myanimal.growth_step == myanimal.animal.max_step 
         return "error_9_1".to_json #fully upgraded and cannot further upgrade
     end
 
@@ -223,13 +195,8 @@ post '/animal_upgrade' do #error9
 end
 
 #HHS
-# have to create Class(Notice), Field(title, created_at, content)
 get '/get_notice' do
-    if Notice.nil?
-        return error_10_1.to_json
-    else
-        return Notice.all.to_json
-    end
+    return Notice.all.to_json
 end
 
 # HHS
@@ -247,11 +214,6 @@ post '/secession' do
         user.delete
         return true.to_json 
     end
-end
-
-# HHS
-post '/notification' do
-    # in App
 end
 
 #finding upgrade password #wjchung
